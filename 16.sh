5fd5a109d01a384b641f1b6924f9ebb02a50db94
starkvps007 <168563466+starkvps007@users.noreply.github.com>
2025-01-19 22:46:43 +0900

    Update 16.sh

--- 16.sh
+++ 16.sh
@@ -80,7 +80,7 @@ echo "Creating instances with the AMI: $AMI_ID..."
 
 aws ec2 run-instances \
     --image-id "$AMI_ID" \
-    --count 8 \
+    --count 16 \
     --instance-type "$INSTANCE_TYPE" \
     --key-name "$KEY_NAME" \
     --security-groups "$SECURITY_GROUP" \
